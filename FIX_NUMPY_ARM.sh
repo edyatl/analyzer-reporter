16b70ee92de3a45f3672b69be616dcf6fd6f8fc1
edyatl <edyatl@yandex.ru>
2024-03-19 18:25:44 +0300

    Fix 2 to cure file for NumPy on ARM.

--- FIX_NUMPY_ARM.sh
+++ FIX_NUMPY_ARM.sh
@@ -9,7 +9,7 @@ check_symlinks() {
     local sys_dir="/usr/lib/python3/dist-packages"
 
     # Check if symbolic links exist for the package
-    if [ -e "$directory/$package_name"* ]; then
+    if ls "$directory/$package_name"* 1> /dev/null 2>&1 ; then
         echo "Symbolic links for $package_name already exist in $directory. Skipping."
     else
         ln -s "$sys_dir/$package_name"* "$directory/" || echo "Can't create symbolic links"
@@ -19,7 +19,7 @@ check_symlinks() {
 # Step 1: Install system package python3-numpy from Debian's repositories
 echo "Installing system package python3-numpy..."
 sudo apt-get update || echo "Can't update package list"
-sudo apt-get install -y python3-numpy python3-pandas python3-scipy || echo "Can't install packages"
+sudo apt-get install -y python3-numpy python3-pandas python3-scipy python3-matplotlib || echo "Can't install packages"
 
 # Step 2: Activate virtual environment at '../venv' path
 echo "Activating virtual environment..."
@@ -29,12 +29,14 @@ source ../venv/bin/activate || echo "Can't activate virtual environment"
 echo "Uninstalling numpy package from virtual environment..."
 pip uninstall -y numpy || echo "Can't uninstall numpy package"
 pip uninstall -y pandas || echo "Can't uninstall pandas package"
-pip uninstall -y scipy || echo "Can't uninstall pandas package"
+pip uninstall -y scipy || echo "Can't uninstall scipy package"
+pip uninstall -y matplotlib || echo "Can't uninstall matplotlib package"
 
 # Step 4: Create symbolic links from system python3-numpy to virtual environment
 echo "Creating symbolic links..."
 check_symlinks numpy
 check_symlinks pandas
 check_symlinks scipy
+check_symlinks matplotlib
 
 echo "Fixing NumPy issue completed successfully!"
