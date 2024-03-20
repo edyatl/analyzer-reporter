bdbc5c540f8725b746ff56694d87ede886c79291
edyatl <edyatl@yandex.ru>
2024-03-20 14:08:30 +0300

    Added real capture with sigrok-cli.

--- analyzer_controller.py
+++ analyzer_controller.py
@@ -9,6 +9,8 @@
 # This file is part of the analyzer_reporter project
 
 import os
+import io
+import subprocess
 import pandas as pd
 
 from config import Configuration as cfg
@@ -33,16 +35,29 @@ class AnalyzerController:
         If real capturing is not available yet, loads sample data from file.
         """
         if self.real_capture:
-            # Perform real capturing using sigrok-cli
-            # Capture signals with sigrok-cli command
-            # Replace the following line with the actual sigrok-cli command for capturing signals
-            self.logger.debug(
-                "Start block with sigrok-cli. Real captur: %s", self.real_capture
-            )
-            # Returns empty DataFrame
-            return pd.DataFrame()
+            # Perform real capturing using sigrok-cli and store the output in a buffer
+            command = [
+                "sigrok-cli",
+                "--driver",
+                "hantek-4032l",
+                "--channels",
+                "A0=AS4_2,A1=AS4_4,A2=AS3_2,A3=AS3_4,A4=AS6_2,A5=AS6_4,A6=AS7_2,A7=AS7_4,A8=AS1_4,A9=AS2_4",
+                "--output-format",
+                "csv",
+                "--config",
+                "samplerate=1000",
+                "--samples",
+                "2048",
+            ]
+            process = subprocess.Popen(command, stdout=subprocess.PIPE)
+            output, _ = process.communicate()
+            output_str = output.decode("utf-8")
+
+            # Convert the output to a pandas DataFrame
+            df = pd.read_csv(io.StringIO(output_str))
+            return df
         else:
-            # Load sample data from exaplle data file
+            # Load sample data from example data file
             df = pd.read_csv(self.data_path)
             self.logger.debug("Data loaded from file: %s", self.data_path)
             return df
